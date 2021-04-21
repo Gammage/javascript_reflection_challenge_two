510fdd3bf1aa8345ac71f16813a6db6f749c9b48
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-04-21 14:49:44 +0100

    changes

---
+++ javaScript/main.js
@@ -0,0 +1,12 @@
+//event listener(s)
+
+document.getElementById('email_submit').addEventListener('click');
+
+// random image api
+
+// refresh image src
+const refreshImage = document.getElementById("refresh");
+document.getElementById('skip_btn').addEventListener('click')
+
+
+

9b5868a85ebfc65b94bf023f4745c27c719ccd63
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-04-23 09:19:09 +0100

    Update main.js

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,26 +1,38 @@
-//GET URL
-let imageUrl = 'https://picsum.photos/300';
+//Get img with axios
 
-//assign url to image element on desktop
-
-
-
-
-// document.getElementById('email_submit').addEventListener('click');
-
-// random image api
-let skip = false;
-//SKIP
-document.getElementById('skip_btn').addEventListener('click', function() {
-    skip = !skip;
-
-    if(skip) { document.getElementById("refresh").src= imageUrl;
-        console.log('click'); }
-});
+const getImage = () => {
+    axios.get("https://picsum.photos/300")
+    .then(response => {
+        pickImage(response.data);
+        console.log('test');
+    })
+    .catch(err => errorHandler(err))
+};
 
 
-//ADD 
-document.getElementById('add_btn').addEventListener('click', function() {
 
-})
+// let imageUrl = 'https://picsum.photos/300';
+
+// //assign url to image element on desktop
+
+
+
+
+// // document.getElementById('email_submit').addEventListener('click');
+
+// // random image api
+// let skip = false;
+// //SKIP
+// document.getElementById('skip_btn').addEventListener('click', function() {
+//     skip = !skip;
+
+//     if(skip) { document.getElementById("refresh").src= imageUrl;
+//         console.log('click'); }
+// });
+
+
+// //ADD 
+// document.getElementById('add_btn').addEventListener('click', function() {
+
+// })
 

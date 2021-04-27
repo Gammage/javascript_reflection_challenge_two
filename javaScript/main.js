b3f4543a3c50f605146ad5fd7e10b5eab0a162d5
Benjamin Gammage <benjamin.gammage@gmail.com>
2021-04-27 21:07:20 +0100

    email validation done

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,61 +1,43 @@
 const skipButton = document.getElementById("skip_btn");
 const addButton = document.getElementById("add_btn");
+let emailText = document.getElementById('email_text');
 //Get img with axios
 
-
-    const getimage = () => axios.get("https://picsum.photos/300")
-    .then(response => {
-        pictureId = response.headers['picsum-id'];
-        console.log(response);
-        imageUrl = `https://picsum.photos/id/${pictureId}/300`;
-        document.getElementById("image_src").src = imageUrl;
-    })
-    .catch(function (error) {
-        console.log(error)
-    });
-
+let usedEmails = [];
+let correctEmail
     
+const getimage = () => axios.get("https://picsum.photos/300")
+.then((response) => {
+    pictureId = response.headers['picsum-id'];
+    console.log(response);
+    imageUrl = `https://picsum.photos/id/${pictureId}/300`;
+    document.getElementById("image_src").src = imageUrl;
+})
+.catch(function (error) {
+    console.log(error)
+});
 
-// let tempImages = [pictureId];
-// console.log(tempImages);
+addButton.addEventListener('click', function() {
+    validateEmail(emailText.value);
+});
+
+function validateEmail(emailInput){
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (emailInput.match(mailFormat)) {
+
+        addimgtobox(emailInput, imageUrl)
+        createBoxforImg()
+
+    } else {
+        // emailInputError.textContent = emailerrorstring
+        console.log("error");
+    }
+}
 
 window.addEventListener("load", getimage);
 skipButton.addEventListener("click", getimage);
 
-// let urlArray = [pictureId];
-// let imageArray = [ImageUrl];
+function createBoxforImg () {};
+function addImgetoBox () {};
 
-// const tempimages = ['Id', 'Url'];
-// console.log(tempimages);
-
-// const save = () => {
-//     tempimages.push(ImageUrl);
-//     tempimages.push(pictureId);
-// }
-
-// class Useremail {
-//     constructor(id, email){
-//         this.id = id;
-//         this.email = email;
-//     }
-// }
-
-// var emailAddress = new Useremail {}
-
-// the new keyword
-    // - creates a new empty object
-    // - sets the value of this to be the new empty object
-    // - calls the constructor method
-
-// addButton.addEventListener("click", save);
-
-// add image to temprorary array
-
-// array of images added
-
-
-// create array
-// const selectedImages
-
-//class email > ID AND URL
 
